f1ae545131627020c21359bcb5e0ce0adba296a4
ajmdz <contact.ajmendoza@gmail.com>
2023-12-16 06:31:23 +0800

    Train using RNN

--- app.py
+++ app.py
@@ -2,31 +2,31 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 import joblib
+import pickle
 
-# pipe_lr = joblib.load(open("models/emotion_classifier_pipeline.pkl"))
-with open("model/emotion_classifier_pipeline.pkl", "rb") as pipeline_file:
-    pipe_lr = joblib.load(pipeline_file)
+with open("tokenizer.pkl", "rb") as file:
+    tokenizer = pickle.load(file)
+
+model = tf.keras.models.load_model("model.h5")
+
+class_dict = {0: 'fear', 1: 'sadness', 2: 'anger', 3: 'joy'}
 
 gif_dict = {
     'anger': 'https://media.giphy.com/media/l1J9u3TZfpmeDLkD6/giphy.gif',
-    'disgust': 'https://media.giphy.com/media/xUA7bcUzBNSLC8Zy3C/giphy.gif',
     'fear': 'https://media.giphy.com/media/bEVKYB487Lqxy/giphy.gif',
     'joy': 'https://media.giphy.com/media/12XTNObsY1pWQU/giphy.gif',
-    'neutral': 'https://media.giphy.com/media/XbgZvND0TzFMUFobmI/giphy.gif',
-    'sadness': 'https://media.giphy.com/media/9Y5BbDSkSTiY8/giphy.gif',
-    'shame': 'https://media.giphy.com/media/yjGdFXbm8KpXF5Xqco/giphy.gif',
-    'surprise': 'https://media.giphy.com/media/oYtVHSxngR3lC/giphy.gif'
-
+    'sadness': 'https://media.giphy.com/media/9Y5BbDSkSTiY8/giphy.gif'
 }
 
 def predict_emotion(text):
-    results = pipe_lr.predict([text])
-    return results[0]
-
-def get_prediction_proba(text):
-    results = pipe_lr.predict_proba([text])
-    return results
+    tokenized_input = tokenizer.texts_to_sequences([text])
+    padded_input = tf.keras.preprocessing.sequence.pad_sequences(tokenized_input, truncating='post', padding='post', maxlen=50)
+    output = model.predict(padded_input)
+    predicted = class_dict[int(tf.argmax(output, axis=1).numpy()[0])]
+    # predicted = class_dict[output.argmax(axis=1)[0]]
+    return predicted, output
 
 # emotions_emoji_dict
 
@@ -42,8 +42,7 @@ def main():
     if submit_text:
         col1, col2 = st.columns(2)
 
-        prediction = predict_emotion(raw_text)
-        probability = get_prediction_proba(raw_text)
+        prediction, probability = predict_emotion(raw_text)
 
         with col1:
             confidence = f"{np.max(probability)*100:.2f}%"
@@ -53,7 +52,7 @@ def main():
         with col2:
             st.subheader("Prediction Probability")
             # st.write(probability)
-            proba_df = pd.DataFrame(probability, columns=pipe_lr.classes_)
+            proba_df = pd.DataFrame(probability, columns=class_dict.values())
             # st.write(proba_df.T)
             proba_df_clean = proba_df.T.reset_index()
             proba_df_clean.columns = ["emotions", "probability"]
